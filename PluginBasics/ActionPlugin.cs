b80fdc981e7a04999c0ff441898f0de7d8e595e2
Shah Alam <shah.alam@verzonal.com>
2023-07-04 17:07:48 +0530

    Add project files.

---
+++ PluginBasics/ActionPlugin.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Microsoft.Xrm.Sdk;
+using System.ServiceModel;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization.Json;
+using PluginBasics;
+
+namespace Plugins
+{
+    public class ActionPlugin : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            ITracingService tracingService = null;
+            IOrganizationService service = null;
+            try
+            {
+                IExecutionContext context = (IExecutionContext)serviceProvider.GetService(typeof(IExecutionContext));
+                IOrganizationServiceFactory serviceFactory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+                service = serviceFactory.CreateOrganizationService(context.UserId);
+                tracingService = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+                // TODO: Core Logic
+                GetContactRecord(service).GetAwaiter().GetResult();
+            }
+            catch (FaultException<OrganizationServiceFault> ex)
+            {
+                tracingService.Trace("FollowUpPlugin: {0}", ex.ToString());
+                throw new InvalidPluginExecutionException("An error occurred in FollowUpPlugin." + ex.Message, ex);
+            }
+            catch (Exception ex)
+            {
+                tracingService.Trace("FollowUpPlugin: {0}", ex.ToString());
+                throw;
+            }
+        }
+
+        public async Task GetContactRecord(IOrganizationService service)
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage(HttpMethod.Get, "https://iqtr.crm.dynamics.com/api/data/v9.2/contacts(F5973462-768E-EB11-B1AC-000D3AE92B46)");
+            request.Headers.Add("Authorization", "Bearer " + await GetCRMToken());
+            var response = await client.SendAsync(request);
+            response.EnsureSuccessStatusCode();
+            string jsonData = await response.Content.ReadAsStringAsync();
+            Entity caseRecord = new Entity("incident");
+            caseRecord["title"] = "Case Created By Action Pluin";
+            caseRecord["description"] = jsonData.Substring(0, 100);
+            caseRecord["customerid"] = new EntityReference("contact", new Guid("F5973462-768E-EB11-B1AC-000D3AE92B46"));
+            service.Create(caseRecord);
+        }
+
+        public async Task<string> GetCRMToken()
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage(HttpMethod.Post, "https://login.microsoftonline.com/5af1f8cd-bc52-4bf7-af52-6a75b233e91a/oauth2/token");
+            request.Headers.Add("Cookie", "fpc=AksxMfvn63hGpr7Co2Z_Myq5VxC7AQAAAD3vNdwOAAAA; stsservicecookie=estsfd; x-ms-gateway-slice=estsfd");
+            var content = new MultipartFormDataContent();
+            content.Add(new StringContent("client_credentials"), "grant_type");
+            content.Add(new StringContent("https://iqtr.crm.dynamics.com"), "resource");
+            content.Add(new StringContent("83836b76-ff49-4515-bbae-a576af44fd99"), "client_id");
+            content.Add(new StringContent("fpp8Q~aXoUH0xJVSlJa0oOEKldqIColzEUhiwbox"), "client_secret");
+            request.Content = content;
+            var response = await client.SendAsync(request);
+            response.EnsureSuccessStatusCode();
+            string stringData = await response.Content.ReadAsStringAsync();
+            CRMToken token = Helper.ConvertIntoObject<CRMToken>(stringData);
+            return token.access_token;
+        }
+    }
+}
---
+++ PluginBasics/Models.cs
@@ -0,0 +1,48 @@
+﻿using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+
+namespace PluginBasics
+{
+    public static class Helper
+    {
+        public static T ConvertIntoObject<T>(string ResponseString)
+        {
+            using (MemoryStream DeSerializememoryStream = new MemoryStream())
+            {
+                //initialize DataContractJsonSerializer object and pass Student class type to it
+                DataContractJsonSerializer serializer = new DataContractJsonSerializer(typeof(T));
+
+                //user stream writer to write JSON string data to memory stream
+                StreamWriter writer = new StreamWriter(DeSerializememoryStream);
+                writer.Write(ResponseString);
+                writer.Flush();
+
+                DeSerializememoryStream.Position = 0;
+                //get the Desrialized data in object of type Student
+                T SerializedObject = (T)serializer.ReadObject(DeSerializememoryStream);
+                return SerializedObject;
+            }
+        }
+    }
+
+    [DataContract]
+    public class CRMToken
+    {
+        [DataMember]
+        public string token_type { get; set; }
+        [DataMember]
+        public string expires_in { get; set; }
+        [DataMember]
+        public string ext_expires_in { get; set; }
+        [DataMember]
+        public string expires_on { get; set; }
+        [DataMember]
+        public string not_before { get; set; }
+        [DataMember]
+        public string resource { get; set; }
+        [DataMember]
+        public string access_token { get; set; }
+    }
+
+}
